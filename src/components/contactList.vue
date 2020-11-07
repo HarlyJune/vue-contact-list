bd5ced116069b28cbd13dab2afab56f18a6af32e
HarlyJune <litvinchuk03@gmail.com>
2020-11-07 15:36:22 +0300

    adding changeButton and add forms

--- src/components/contactList.vue
+++ src/components/contactList.vue
@@ -2,46 +2,45 @@
 import delButton from "./delButton";
 import contact from "./contactShort";
 export default {
-    props: ["contacts"],
-    mounted: function(){
+  props: ["contacts"],
+  mounted: function () {},
+  components: {
+    Contact: contact,
+    DelButton: delButton,
+  },
+  methods: {
+    goToContactView(contact) {
+      console.log("go");
+      this.$router.push({
+        name: "Contact",
+        params: { contact: contact },
+      });
     },
-    components: {
-      Contact: contact,
-      DelButton: delButton,
-    },
-    methods: {
-        goToContactView(contact){
-            console.log("go")
-            this.$router.push({
-                name: 'Contact',
-                params: { contact : contact }
-            });
-        }
-    }
+  },
 };
 </script>
 
 <template>
-    <div>
-        <button>Add contact</button>
-        <div  v-for="contact in contacts"
-              :key="contact.id"
-              class="container">
-            <Contact
-                    :contact="contact"
-                    @click.native="goToContactView(contact)"
-            />
-            <DelButton  :id="contact.id" />
-            <!-- c помощью v-for итерирую массив данных и передаю его в компонент contact через v-bind
+  <div>
+    <form action="/action_page.php">
+      <label for="fname">First name:</label>
+      <input type="text" id="fname" name="fname" /><br /><br />
+      <label for="lname">Last name:</label>
+      <input type="text" id="lname" name="lname" /><br /><br />
+      <input type="submit" value="Add Contact!" />
+    </form>
+    <div v-for="contact in contacts" :key="contact.id" class="container">
+      <Contact :contact="contact" @click.native="goToContactView(contact)" />
+      <DelButton :id="contact.id" />
+      <!-- c помощью v-for итерирую массив данных и передаю его в компонент contact через v-bind
              (key обязателен, т.к. без него компилятор ругается)-->
-        </div>
     </div>
+  </div>
 </template>
 
 <style >
-  .container{
-    display: flex;
-    flex-direction: column;
-    justify-content: start;
-  }
+.container {
+  display: flex;
+  justify-content: start;
+}
 </style>
--- src/components/contactView.vue
+++ src/components/contactView.vue
@@ -1,31 +1,36 @@
 <script>
-    export default ({
-        props: ["contact"]
-    })
+import delButton from "./delButton";
+import changeButton from "./changeButton";
+export default {
+  props: ["contact"],
+  components: {
+    DelButton: delButton,
+    ChangeButton: changeButton
+  },
+};
 </script>
 
 <template>
+  <div>
     <div>
-        <div v-for="key in Object.keys(contact)"
-             :key="key"
-             class="row"
-        >
-            {{key}} : {{contact[key]}}
-
-        </div>
-
+      <ChangeButton />
+      <DelButton />
     </div>
+    <div v-for="key in Object.keys(contact)" :key="key" class="row">
+      {{ key }} : {{ contact[key] }}
+    </div>
+  </div>
 </template>
 
 <style scoped>
-    .row{
-        display: flex;
-        flex-direction: row;
-        width: 100%;
-        height: 6em;
-        border: 1px solid tomato;
-        border-radius: 14px;
-    }
+.row {
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: 6em;
+  border: 1px solid tomato;
+  border-radius: 14px;
+}
 </style>
 
 
